144e9b6cd8ac0cb87588721d1ff492396a453788
Safi <201b366@juetguna.in>
2022-05-27 11:17:05 +0530

    commit

--- app/src/main/java/com/example/ai_mad_numgame/MainActivity.java
+++ app/src/main/java/com/example/ai_mad_numgame/MainActivity.java
@@ -24,7 +24,7 @@ import java.util.HashSet;
 import java.util.Random;
 import java.util.Set;
 
-public class MainActivity extends AppCompatActivity {
+public class  MainActivity extends AppCompatActivity {
     SharedPreferences sharedPreferences;
     int matchCounter=0;
     int []performance={-1,-1,-1,-1,-1,-1}; //score of a game is updated in this array
@@ -72,12 +72,59 @@ public class MainActivity extends AppCompatActivity {
     public void newMatch() {  //A game is composed of three matches
 
         int operand1 = random.nextInt(10);
-        int operand2=0;
+        int operand2 = 0;
         //check is operand2 is not zero; otherwise in case of division-divide by zero error will come
         String operator = operators[random.nextInt(4)];
         textView2.setText(operand1 + operator + operand2);
 
-      // Your code here, to diplay correct and incorrect options on the buttons
+
+        int correctanswer = -100;
+
+        if (operator.equals("+"))
+            correctanswer = operand1 + operand2;
+
+        else if (operator.equals("-"))
+            correctanswer = operand1 - operand2;
+
+        else if (operator.equals("*"))
+            correctanswer = operand1 * operand2;
+
+        else
+            correctanswer = operand1 / operand2;
+
+
+
+
+        correctButton = random.nextInt(4);
+
+        if (correctButton==0) {
+            button1.setText(correctanswer + " ");
+            button2.setText(correctanswer + " ");
+            button3.setText(correctanswer + " ");
+            button4.setText(correctanswer + " ");
+        }
+        else if (correctanswer==1) {
+
+
+            button1.setText(correctanswer + 1 + " ");
+            button2.setText(correctanswer + " ");
+            button3.setText(correctanswer - 1 + " ");
+            button4.setText(correctanswer + 2 + " ");
+        }
+
+            else if (correctanswer==2) {
+                button1.setText(correctanswer +1+ " ");
+            button2.setText(correctanswer -1+ " ");
+            button3.setText(correctanswer + " ");
+            button4.setText(correctanswer +2+ " ");
+        }
+            else if (correctanswer==4) {
+            button1.setText(correctanswer +1+ " ");
+            button2.setText(correctanswer +2+ " ");
+            button3.setText(correctanswer -1+ " ");
+            button4.setText(correctanswer + " ");
+
+        }
 
         if(matchCounter==3){    // if three matches are completed updatee the perfomrance in sharedpreferences
 
@@ -96,6 +143,8 @@ public class MainActivity extends AppCompatActivity {
         //Computing the sum of score array, which has the 1 or in each index,depending on correct or incorrect answers
         int sum=0;
        // your code here
+        for(int i=0;i<score.length;i++)
+            sum=sum+score[i];
         return sum;
     }
 
@@ -112,9 +161,28 @@ public class MainActivity extends AppCompatActivity {
         return dataFrame;
     }
 
-    public String getInterpretation(int [][]dataFrame,double slope){
-       //provide interpretation based on your slope analysis
+    public String getInterpretation(int [][]dataFrame,double slope) {
+        //provide interpretation based on your slope analysis
         // Your code here
-        return "Your Interpretation";
+        String Interpretation = " All is well";
+        if (slope > 0 && slope <= .5)
+            Interpretation = " you are a slow learner";
+
+                    else if (slope < .5)
+                Interpretation = " You are a fast learner";
+
+            else if (slope < 0)
+                Interpretation = " You are an Un learner";
+
+            else if(dataFrame [0][1]== 3 && slope ==0)
+                Interpretation = " U achieve perfection";
+
+                else if(dataFrame[0][1] == 0 && slope ==0)
+                    Interpretation = "u Dont learn";
+
+
+
+
+            return Interpretation;
+        }
     }
-}
